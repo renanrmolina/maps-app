e2cd91049d720b740a741b0ce96edb21269feaa4
Renan Molina <renanrobertomolina@hotmail.com>
2023-10-24 22:14:22 -0300

    Criação do mapa ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,43 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  //Pega a div do html e coloca ela na variável mapRef
+  @ViewChild('map') mapRef!: ElementRef;
+
+  //Cria uma variavel para o Maps
+  map!: google.maps.Map;
+
   constructor() {}
 
+    async exibirMapa(){
+       // The location of Uluru
+  const position = { lat: -5.473844, lng: -45.568046 };
+
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
+
+  // The map, centered at Uluru
+  this.map = new Map(
+    this.mapRef.nativeElement,
+    {
+      zoom: 4,
+      center: position,
+      mapId: 'DEMO_MAP_ID',
+    }
+  );
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: this.map,
+    position: position,
+    title: 'Uluru'
+  });
+    }  
+
+    ionViewWillEnter(){
+      this.exibirMapa();
+    }
+ 
 }
